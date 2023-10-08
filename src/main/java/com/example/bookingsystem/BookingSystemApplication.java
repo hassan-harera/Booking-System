7ea4d0477c6fc0463126da8c196c007125efa464
hharera <Hassan.Shaban.Harera@gmail.com>
2023-10-08 21:12:06 +0300

    initial commit

--- src/main/java/com/example/bookingsystem/BookingSystemApplication.java
+++ src/main/java/com/example/bookingsystem/BookingSystemApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BookingSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookingSystemApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookingSystemApplication.class, args);
+    }
 
 }
